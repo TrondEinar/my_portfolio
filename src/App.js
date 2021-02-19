3897a05c5cd87a67628a4ea789dd4ac3b28d4ebc
TrondEinar <trond-einar98@hotmail.no>
2021-02-19 11:44:20 +0100

    added styles for sidebar and main-content

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+
+function App() {
+    return (
+        <div className="App">
+            <div className="sidebar"></div>
+            <div className="main-content"></div>
+        </div>
+    );
+}
+
+export default App;
