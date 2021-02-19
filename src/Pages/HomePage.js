78de5fe5ed98b8a32af16a2116a016151cdc1b20
TrondEinar <trond-einar98@hotmail.no>
2021-02-19 14:39:47 +0100

    added icons and styles to the homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
     return (
-        <div>
-            <h1>HomePage</h1>
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                    Hei! Mitt navn er
+                    <span> Trond Einar Eriksen.</span>
+                </h1>
+                <p className="h-sub-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Cupiditate facere, numquam vitae placeat consequatur corrupti officia 
+                    quibusdam. Blanditiis doloremque nemo ex facilis neque. 
+                    A sint ipsam earum nemo omnis et.
+                </p>
+                <div className="icons">
+                    <Link className="icon-holder">
+                    <FontAwesomeIcon icon={faFacebook} className="icon fb"/>
+                    </Link>
+                    <Link className="icon-holder">
+                    <FontAwesomeIcon icon={faGithub} className="icon gh"/>
+                    </Link>
+                    <Link className="icon-holder">
+                    <FontAwesomeIcon icon={faLinkedin} className="icon li"/>
+                    </Link>
+                </div>
+            </header>
         </div>
-    );
+    )
 }
 
 export default HomePage;
