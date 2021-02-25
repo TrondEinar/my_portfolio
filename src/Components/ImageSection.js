538ed8dbe8a2333cf58a94ea21780a8c5db0ece1
TrondEinar <trond-einar98@hotmail.no>
2021-02-25 17:40:57 +0100

    added imagesection-component and style

---
+++ src/Components/ImageSection.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import about from '../img/About.png';
+
+function ImageSection() {
+    return (
+        <div className="ImageSection">
+            <div className="img">
+                <img src={about} alt=""/>
+            </div>
+            <div className="about-info">
+                <h4>I am<span> Trond Einar Eriksen</span></h4>
+                <p className="about-text">
+                    Loren ipsum dolor sitt amet concenet aididigff elite.
+                    VOlftj jfjfksklkfk pfkfkrgf jdkfkjf kdlfdfj kdfjdk fkdfjkd.
+                    Desire per lo la ta dooso vi slone ve?
+                </p>
+                <div className="about-details">
+                    <div className="left-section">
+                        <p>Full Name</p>
+                        <p>Age</p>
+                        <p>Nationality</p>
+                        <p>Languages</p>
+                        <p>Address</p>
+                        <p>Countries</p>
+                    </div>
+                    <div className="right-section">
+                        <p>: Trond Einar Eriksen</p>
+                        <p>: 22</p>
+                        <p>: Norwegian</p>
+                        <p>: Norwegian, English, German</p>
+                        <p>: Brannvålsveien 1009, 2560 Alvdal, Norway</p>
+                        <p>: Lorem Ipsum</p>
+                    </div>
+                </div>
+                <button className="btn">Download CV</button>
+            </div>
+        </div>
+    )
+}
+
+export default ImageSection;
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Tittle from '../Components/Tittle';
+import ImageSection from '../Components/ImageSection';
 
 function AboutPage() {
     return (
         <div className="AboutPage">
             <Tittle title={'About Me'} span={'About Me'}/>
+            <ImageSection />
         </div>
     )
 }
