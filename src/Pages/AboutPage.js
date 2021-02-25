c49684b4bd961acbe25c220960c63efc1fce2ffd
TrondEinar <trond-einar98@hotmail.no>
2021-02-25 15:43:36 +0100

    created all pages and title-component with styles

---
+++ src/Pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Tittle from '../Components/Tittle';
+
+function AboutPage() {
+    return (
+        <div className="AboutPage">
+            <Tittle title={'About Me'} span={'About Me'}/>
+        </div>
+    )
+}
+
+export default AboutPage;
