3f7fb3e1c76b02ca4f93f3edf7f1dedd04fdf0be
TrondEinar <trond-einar98@hotmail.no>
2021-03-05 16:37:03 +0100

    added skillssection-component and styles

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import Tittle from '../Components/Tittle';
 import ImageSection from '../Components/ImageSection';
+import SkillsSection from '../Components/SkillsSection';
 
 function AboutPage() {
     return (
         <div className="AboutPage">
             <Tittle title={'About Me'} span={'About Me'}/>
             <ImageSection />
+            <Tittle title={'My Skills'} span={'My Skills'}/>
+            <div className="skillsContainer">
+            <SkillsSection skill={'HTML'} progress={'80%'} width={'80%'}/>
+            <SkillsSection skill={'CSS'} progress={'80%'} width={'80%'}/>
+            <SkillsSection skill={'JavaScript'} progress={'50%'} width={'50%'}/>
+            <SkillsSection skill={'React JS'} progress={'60%'} width={'60%'}/>
+            <SkillsSection skill={'Java'} progress={'50%'} width={'50%'}/>
+            <SkillsSection skill={'Kotlin'} progress={'50%'} width={'50%'}/>
+            <SkillsSection skill={'SQL'} progress={'50%'} width={'50%'}/>
+            <SkillsSection skill={'Web Design'} progress={'40%'} width={'40%'}/>
+            <SkillsSection skill={'UI/UX design'} progress={'30%'} width={'30%'}/>
+            </div>
         </div>
     )
 }
