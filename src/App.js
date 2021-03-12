9eeca2434ac7ab99c2fc3fb2135f42714753d89f
TrondEinar <trond-einar98@hotmail.no>
2021-03-12 15:42:05 +0100

    add media queries and nav-toggle

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ import { Switch, Route } from 'react-router-dom';
 import AboutPage from './Pages/AboutPage';
 import PortfoliosPage from './Pages/PortfoliosPage';
 import ContactPage from './Pages/ContactPage';
+import { useState } from 'react';
 
 
 function App() {
+  const [navToggle, setNavToggle] = useState(false);
+
+  const navClick = () => {
+    setNavToggle(!navToggle);
+  }
+
     return (
         <div className="App">
-            <div className="sidebar">
+            <div className={`sidebar ${navToggle ? 'nav-toggle': ''}`}>
                 <NavBar />
+            </div>
+            <div className="nav-btn" onClick={navClick}>
+              <div className="lines-1"></div>
+              <div className="lines-2"></div>
+              <div className="lines-3"></div>
             </div>
             <div className="main-content">
                 <div className="content">
