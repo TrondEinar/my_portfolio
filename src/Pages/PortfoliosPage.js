5f30864cdd01d0499f8e81c15f0b7df0e2599496
TrondEinar <trond-einar98@hotmail.no>
2021-03-12 14:07:50 +0100

    add portfolios-components and styles

--- src/Pages/PortfoliosPage.js
+++ src/Pages/PortfoliosPage.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import { React, useState } from 'react';
+import Title from '../Components/Tittle';
+import Categories from '../Components/Categories';
+import MenuItems from '../Components/MenuItems';
+import portfolios from '../Components/allportfolios';
 
 function PortfoliosPage() {
+    const [categories, setCategories] = useState(null);
+    const [menuItems, setMenuItems] = useState(portfolios);
+
     return (
-        <div>
-            <h1>Portfolios Page</h1>
+        <div className="PortfolioPage">
+            <div className="title">
+            <Title title={'Portfolios'} span={'Portfolios'} />
+            </div>
+            <div className="portfolios-data">
+                <Categories />
+                <MenuItems menuItem={menuItems} />
+            </div>
         </div>
     )
 }
