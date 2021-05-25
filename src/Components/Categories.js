8f482e35068239871ab29863c0661b6c9da52378
TrondEinar <trond-einar98@hotmail.no>
2021-05-25 11:20:41 +0200

    prettier config

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
 
-function Categories({filter, categories}) {
-    return (
-        <div className="buttons">
-            {
-                categories.map((cat, i) => {
-                    return <button className="btn-portfolio" type="button" onClick={() => filter(cat)} key={i}>{cat}</button>
-                })
-            }
-        </div>
-    )
+function Categories({ filter, categories }) {
+  return (
+    <div className="buttons">
+      {categories.map((cat, i) => {
+        return (
+          <button
+            className="btn-portfolio"
+            type="button"
+            onClick={() => filter(cat)}
+            key={i}
+          >
+            {cat}
+          </button>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Categories;
--- src/Pages/PortfoliosPage.js
+++ src/Pages/PortfoliosPage.js
@@ -1,36 +1,39 @@
-import { React, useState } from 'react';
-import Title from '../Components/Tittle';
-import Categories from '../Components/Categories';
-import MenuItems from '../Components/MenuItems';
-import portfolios from '../Components/allportfolios';
+import { React, useState } from "react";
+import Title from "../Components/Tittle";
+import Categories from "../Components/Categories";
+import MenuItems from "../Components/MenuItems";
+import portfolios from "../Components/allportfolios";
 
-const allCategories = ['All', ...new Set(portfolios.map(item => item.category))];
+const allCategories = [
+  "All",
+  ...new Set(portfolios.map((item) => item.category)),
+];
 function PortfoliosPage() {
-    const [categories, setCategories] = useState(allCategories);
-    const [menuItems, setMenuItems] = useState(portfolios);
+  const [categories, setCategories] = useState(allCategories);
+  const [menuItems, setMenuItems] = useState(portfolios);
 
-    const filter = (category) => {
-        if(category === 'All') {
-            return setMenuItems(portfolios);
-        }
-
-        const filteredData = portfolios.filter((item) => {
-            return item.category === category;
-        })
-        setMenuItems(filteredData)
+  const filter = (category) => {
+    if (category === "All") {
+      return setMenuItems(portfolios);
     }
 
-    return (
-        <div className="PortfolioPage">
-            <div className="title">
-            <Title title={'Portfolios'} span={'Portfolios'} />
-            </div>
-            <div className="portfolios-data">
-                <Categories filter={filter} categories={categories} />
-                <MenuItems menuItem={menuItems} />
-            </div>
-        </div>
-    )
+    const filteredData = portfolios.filter((item) => {
+      return item.category === category;
+    });
+    setMenuItems(filteredData);
+  };
+
+  return (
+    <div className="PortfolioPage">
+      <div className="title">
+        <Title title={"Portfolios"} span={"Portfolios"} />
+      </div>
+      <div className="portfolios-data">
+        <Categories filter={filter} categories={categories} />
+        <MenuItems menuItem={menuItems} />
+      </div>
+    </div>
+  );
 }
 
 export default PortfoliosPage;
